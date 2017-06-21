60f3273a1fb0ef02b0ec6bf8e55be8451cca7e03
plliao <r02921027@ntu.edu.tw>
2017-06-21 12:55:00 +0800

    change seq2seq_auto_encoder to seq2vec_R2R_char

--- seq2vec/model/seq2vec_R2R_char.py
+++ seq2vec/model/seq2vec_R2R_char.py
@@ -1,72 +1,21 @@
 """Sequence-to-Sequence Auto Encoder."""
-import numpy as np
-
 import keras.models
-from keras.models import Sequential
+from keras.models import Input
 from keras.optimizers import RMSprop
-from keras.layers import LSTM, RepeatVector
 from keras.layers.embeddings import Embedding
 from keras.layers.wrappers import TimeDistributed
-from keras.layers import Dense, Dropout, Activation
+from keras.layers import Dense
 from keras.models import Model
+
+from yklz import BidirectionalRNNEncoder, RNNDecoder
+from yklz import LSTMPeephole, RNNCell
 
 from seq2vec.transformer import HashIndexTransformer
 from seq2vec.transformer import OneHotEncodedTransformer
-from seq2vec.model import Seq2VecBase
-from seq2vec.model import TrainableInterfaceMixin
-
-def _create_single_layer_seq2seq_model(
-        max_length,
-        max_index,
-        embedding_size,
-        latent_size,
-        learning_rate,
-        rho=0.9,
-        decay=0.01,
-    ):
-
-    model = Sequential()
-    model.add(
-        Embedding(
-            max_index, embedding_size, input_length=max_length,
-            name='embedding', mask_zero=True, dropout=0.2
-        )
-    )
-    model.add(
-        LSTM(
-            units=latent_size, return_sequences=False,
-            name='en_LSTM_1', dropout=0.2, recurrent_dropout=0.3
-        )
-    )
-    model.add(
-        RepeatVector(max_length)
-    )
-    model.add(
-        LSTM(
-            embedding_size, return_sequences=True,
-            name='de_LSTM_1', dropout=0.2, recurrent_dropout=0.3
-        )
-    )
-    model.add(
-        TimeDistributed(Dense(max_index))
-    )
-    model.add(Dropout(0.2))
-    model.add(Activation('softmax'))
-
-    encoder = Model(
-        model.input, model.get_layer('en_LSTM_1').output
-    )
-
-    optimizer = RMSprop(
-        lr=learning_rate,
-        rho=rho,
-        decay=decay,
-    )
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
-    return model, encoder
+from seq2vec.model import TrainableSeq2VecBase
 
 
-class Seq2SeqAutoEncoderUseWordHash(TrainableInterfaceMixin, Seq2VecBase):
+class Seq2VecR2RChar(TrainableSeq2VecBase):
     """Hash words and feed to seq2seq auto-encoder.
 
     Attributes
@@ -86,15 +35,14 @@ class Seq2SeqAutoEncoderUseWordHash(TrainableInterfaceMixin, Seq2VecBase):
             self,
             max_index,
             max_length,
+            encoding_size=100,
             learning_rate=0.0001,
             embedding_size=64,
             latent_size=20,
         ):
         self.max_index = max_index
-        self.max_length = max_length
-        self.learning_rate = learning_rate
         self.embedding_size = embedding_size
-        self.latent_size = latent_size
+        self.encoding_size = encoding_size
 
         self.input_transformer = HashIndexTransformer(
             max_index, max_length
@@ -103,22 +51,107 @@ class Seq2SeqAutoEncoderUseWordHash(TrainableInterfaceMixin, Seq2VecBase):
             max_index, max_length
         )
 
-        model, encoder = _create_single_layer_seq2seq_model(
-            max_length=self.max_length,
-            max_index=self.max_index + 1,
-            embedding_size=self.embedding_size,
-            latent_size=self.latent_size,
-            learning_rate=self.learning_rate,
+        super(Seq2VecR2RChar, self).__init__(
+            max_length,
+            latent_size,
+            learning_rate
         )
-        self.model = model
-        self.encoder = encoder
+
+    def create_model(
+            self,
+            rho=0.9,
+            decay=0.01,
+        ):
+
+        inputs = Input(
+            shape=(
+                self.max_length,
+            )
+        )
+        char_embedding = Embedding(
+            input_dim=self.max_index,
+            output_dim=self.embedding_size,
+            input_length=self.max_length,
+            mask_zero=True,
+        )(inputs)
+
+        encoded = BidirectionalRNNEncoder(
+            RNNCell(
+                LSTMPeephole(
+                    units=self.latent_size,
+                    dropout=0.1,
+                    recurrent_dropout=0.1
+                ),
+                Dense(
+                    units=self.encoding_size // 2,
+                    activation='tanh'
+                ),
+                dense_dropout=0.1
+            )
+        )(char_embedding)
+        decoded = RNNDecoder(
+            RNNCell(
+                LSTMPeephole(
+                    units=self.latent_size,
+                    dropout=0.1,
+                    recurrent_dropout=0.1
+                ),
+                Dense(
+                    units=self.encoding_size,
+                    activation='tanh'
+                ),
+                dense_dropout=0.1
+            )
+        )(encoded)
+        outputs = TimeDistributed(
+            Dense(
+                units=self.max_index,
+                activation='softmax'
+            )
+        )(decoded)
+
+        model = Model(inputs, outputs)
+        encoder = Model(inputs, encoded)
+
+        optimizer = RMSprop(
+            lr=self.learning_rate,
+            rho=rho,
+            decay=decay,
+        )
+        model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+        return model, encoder
+
+    def transform(self, seqs):
+        transformation = super(Seq2VecR2RChar, self).transform(seqs)
+        return transformation[:, 0, :]
+
+    def load_customed_model(self, file_path):
+        return keras.models.load_model(
+            file_path, custom_objects={
+                'BidirectionalRNNEncoder':BidirectionalRNNEncoder,
+                'LSTMPeephole':LSTMPeephole,
+                'RNNDecoder':RNNDecoder,
+                'RNNCell':RNNCell
+            }
+        )
 
     def load_model(self, file_path):
-        self.model = keras.models.load_model(file_path)
+        self.model = self.load_customed_model(file_path)
         self.encoder = Model(
-            self.model.input, self.model.get_layer('en_LSTM_1').output
+            self.model.input,
+            self.model.get_layer(index=2).output
         )
-        self.max_index = self.model.get_layer('embedding').input_dim - 1
+        self.max_index = self.model.get_layer(index=1).input_dim
         self.max_length = self.model.input_shape[1]
-        self.embedding_size = self.model.get_layer('embedding').output_dim
-        self.latent_size = self.model.get_layer('en_LSTM_1').units
+        self.embedding_size = self.model.get_layer(index=1).output_dim
+        self.latent_size = self.model.get_layer(index=2).layer.recurrent_layer.units
+        self.encoding_size = self.model.get_layer(index=2).layer.dense_layer.units * 2
+
+        self.input_transformer = HashIndexTransformer(
+            self.max_index,
+            self.max_length
+        )
+        self.output_transformer = OneHotEncodedTransformer(
+            self.max_index,
+            self.max_length
+        )
--- seq2vec/tests/model/test_seq2vec_R2R_char.py
+++ seq2vec/tests/model/test_seq2vec_R2R_char.py
@@ -1,85 +1,38 @@
 """Test Sequence-to-Sequence auto encoder."""
 from unittest import TestCase
-from unittest.mock import patch
-from os.path import abspath
-from os.path import dirname
-from os.path import join
 import os
 
 import numpy as np
-from yoctol_utils.hash import consistent_hash
+from seq2vec.model import Seq2VecR2RChar
+from .test_seq2vec_base import TestSeq2VecBaseClass
 
-from seq2vec.model import Seq2SeqAutoEncoderUseWordHash
-from seq2vec.transformer import HashIndexTransformer
-from seq2vec.transformer import OneHotEncodedTransformer
-from seq2vec.util import DataGenterator
-
-class TestSeq2SeqAutoEncoderUseWordHash(TestCase):
+class TestSeq2VecR2RCharClass(TestSeq2VecBaseClass, TestCase):
 
     def setUp(self):
         self.max_index = 10
-        self.max_length = 5
-        self.latent_size = 20
-        self.transformer = Seq2SeqAutoEncoderUseWordHash(
+        super(TestSeq2VecR2RCharClass, self).setUp()
+
+    def create_model(self):
+        return Seq2VecR2RChar(
             max_index=self.max_index,
             max_length=self.max_length,
             latent_size=self.latent_size,
+            encoding_size=self.encoding_size
         )
-        self.train_seq = [
-            ['我', '有', '一個', '蘋果'],
-            ['我', '有', '一支', '筆'],
-            ['我', '有', '一個', '鳳梨'],
-        ]
-        self.test_seq = [
-            ['我', '愛', '吃', '鳳梨'],
-        ]
-        current_path = abspath(__file__)
-        self.dir_path = dirname(current_path)
 
-    @patch('keras.models.Model.fit')
-    def test_correct_latent_size(self, _):
-        self.transformer.fit(self.train_seq)
-        result = self.transformer.transform(self.test_seq)
-        self.assertEqual(result.shape[1], self.latent_size)
+    def test_load_save_model(self):
+        answer = self.model(self.test_seq)
 
-    @patch('keras.models.Model.fit')
-    def test_load_save_model(self, _):
-        model_path = join(self.dir_path, 'auto_encoder_model.h5')
-
-        self.transformer.fit(self.train_seq)
-        answer = self.transformer.transform(self.test_seq)
-
-        self.transformer.save_model(model_path)
-        new_model = Seq2SeqAutoEncoderUseWordHash(
-            max_index=self.max_index,
-            max_length=self.max_length
+        self.model.save_model(self.model_path)
+        new_model = Seq2VecR2RChar(
+            max_index=1,
+            max_length=1
         )
-        new_model.load_model(model_path)
-        result = new_model.transform(self.test_seq)
+        new_model.load_model(self.model_path)
+        self.assertEqual(self.max_index, new_model.max_index)
+        self.assertEqual(self.max_length, new_model.max_length)
+        self.assertEqual(self.latent_size, new_model.latent_size)
+        self.assertEqual(self.encoding_size, new_model.encoding_size)
+        result = new_model(self.test_seq)
         np.testing.assert_array_almost_equal(answer, result)
-        os.remove(model_path)
-
-    @patch('keras.models.Model.fit_generator')
-    def test_fit_generator(self, _):
-        data_path = join(self.dir_path, 'test_corpus.txt')
-
-        x_transformer = HashIndexTransformer(
-            self.max_index, self.max_length
-        )
-        y_transformer = OneHotEncodedTransformer(
-            self.max_index, self.max_length
-        )
-
-        train_data_generator = DataGenterator(
-            data_path, x_transformer, y_transformer, batch_size=10
-        )
-        test_data_generator = DataGenterator(
-            data_path, x_transformer, y_transformer, batch_size=10
-        )
-
-        self.transformer.fit_generator(
-            train_data_generator, test_data_generator, batch_number=2
-        )
-
-        result = self.transformer(self.train_seq)
-        self.assertEqual(result.shape[1], self.latent_size)
+        os.remove(self.model_path)
