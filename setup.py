7a51233a1ad34cb8d7f901af2d6f80ca37b7cf6c
plliao <r02921027@ntu.edu.tw>
2017-06-26 11:19:03 +0800

    release 0.4.0

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ except IOError:
 
 setup(
     name='seq2vec',
-    version='0.2.0',
-    description='A pip package',
+    version='0.4.0',
+    description='A package to turn sequence of words'
+    'into a fix-length representation vector',
     license='GNU 3.0',
     author='cph',
     packages=find_packages(),
@@ -18,7 +19,9 @@ setup(
         'keras',
         'yoctol_utils',
         'gensim',
-        'h5py'
+        'h5py',
+        'yoctol_keras_layer_zoo',
+        'tensorflow'
     ],
     long_description=long_description,
     classifiers=[
